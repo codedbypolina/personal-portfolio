abedfa4cf289a589333c0a5eb0f1ff7f7466e63c
Polina Shliakhina <98726255+codedbypolina@users.noreply.github.com>
2022-06-19 21:19:19 -0400

    Type Animation (JS)

---
+++ js/javascript.js
@@ -0,0 +1,27 @@
+function animation() {
+  const texts = [
+    "Language Lover       ",
+    "Enthusiastic Learner       ",
+    "Web Designer          ",
+  ];
+  let index = 0;
+  let count = 0;
+  let currentText = "";
+  let letter = "";
+
+  (function type() {
+    if (index === texts.length) {
+      index = 0;
+    }
+    currentText = texts[index];
+    letter = currentText.slice(0, ++count);
+
+    document.querySelector(".typing").textContent = letter;
+    if (letter.length === currentText.length) {
+      index++;
+      count = 0;
+    }
+    setTimeout(type, 180);
+  })();
+}
+animation();
