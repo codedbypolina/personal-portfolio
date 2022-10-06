41256bd8fccb41e64772833fb077e572d5f28f4c
Polina Shliakhina <98726255+codedbypolina@users.noreply.github.com>
2022-10-05 21:33:51 -0400

    Add comments to clarify a code

--- js/javascript.js
+++ js/javascript.js
@@ -9,13 +9,17 @@ function animation() {
   let currentText = "";
   let letter = "";
 
+  // THOUGHTS: I think this function could be replaced by loop - this will shorten the code and make it more efficient & faster.
+
   (function type() {
+    // set index to zero once index number is equal to the lenght of "texts" array
     if (index === texts.length) {
       index = 0;
     }
+
     currentText = texts[index];
     letter = currentText.slice(0, ++count);
-
+    // console.log(letter); <DELETE ONCE POSTED)
     document.querySelector(".typing").textContent = letter;
     if (letter.length === currentText.length) {
       index++;
